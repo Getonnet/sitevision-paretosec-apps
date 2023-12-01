4b2e8a65189860be392f26823ab9bb57bdabf8c5
walidmahade <walidmahade@gmail.com>
2023-12-01 10:14:02 +0600

    update site name

--- megamenu-v2/src/actions/getTickers.tsx
+++ megamenu-v2/src/actions/getTickers.tsx
@@ -2,24 +2,30 @@ import requester from "@sitevision/api/client/requester";
 import { useState } from "react";
 
 export const getTickers = () => {
-    const API_URL_PREFIX = window.location.origin;
-    const REST_API = "/rest-api/1/0";
-    const PAGE_REPOSITORY = "/Pareto%20AS/Page%20Repository/Site%20Page";
-    const API_URL_SUFFIX = "/nodes?format=json&json=%7B%22properties%22%3A%5B%22*%22%5D%7D";
-    const PATH_TICKERS = "/Tickers";
+  const API_URL_PREFIX = window.location.origin;
+  const REST_API = "/rest-api/1/0";
+  const PAGE_REPOSITORY = "/2.54e1ff71188bd8464773cc67_sitePage";
+  const API_URL_SUFFIX =
+    "/nodes?format=json&json=%7B%22properties%22%3A%5B%22*%22%5D%7D";
+  const PATH_TICKERS = "/Tickers";
 
-    const [tickers, setTickers] = useState<any>();
+  const [tickers, setTickers] = useState<any>();
 
-    requester
-        .doGet({
-            url: API_URL_PREFIX + REST_API + PAGE_REPOSITORY  + PATH_TICKERS + API_URL_SUFFIX,
-        })
-        .then((response) => {
-            setTickers(response);
-        })
-        .catch((error) => {
-            setTickers([]);
-        })
+  requester
+    .doGet({
+      url:
+        API_URL_PREFIX +
+        REST_API +
+        PAGE_REPOSITORY +
+        PATH_TICKERS +
+        API_URL_SUFFIX,
+    })
+    .then((response) => {
+      setTickers(response);
+    })
+    .catch((error) => {
+      setTickers([]);
+    });
 
-    return tickers
-}
+  return tickers;
+};
