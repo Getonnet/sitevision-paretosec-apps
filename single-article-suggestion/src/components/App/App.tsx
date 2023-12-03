e62fe8629b933948a2f04f9a1b15e004dd1e6cb6
walidmahade <walidmahade@gmail.com>
2023-12-03 15:36:21 +0600

    added placeholder component

--- single-article-suggestion/src/components/App/App.tsx
+++ single-article-suggestion/src/components/App/App.tsx
@@ -2,16 +2,27 @@ import * as React from "react";
 import styles from "./App.scss";
 
 export interface AppProperties {
-  message: string;
-  name: string;
+  articleId: string;
 }
 
-const App: React.FunctionComponent<AppProperties> = ({ message, name }) => {
+const App: React.FunctionComponent<AppProperties> = ({ articleId }) => {
   return (
     <div className={styles.container}>
-      <p className={styles.text}>
-        {message} {name}
-      </p>
+      <div
+        className={styles.thumbnail}
+        style={{
+          backgroundImage: `url("https://use-pareto.sitevision-cloud.se/images/18.4857b8d018b84b042083f70c/1699347304043/Valutarisiko-169.jpg")`,
+          backgroundSize: "cover",
+        }}
+      />
+
+      <div className={styles.content}>
+        <p className={styles.text}>Les også</p>
+        <h2 className={styles.title}>
+          Prøv oss ut i tre måneder med våre beste betingelser, full
+          analysetilgang og populære Infront Web Trader. →
+        </h2>
+      </div>
     </div>
   );
 };
