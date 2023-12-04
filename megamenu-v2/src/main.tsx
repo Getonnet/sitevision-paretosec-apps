59af361ff29a46d5e8bfc7a7658f36afd6442610
Eric <ericjohnariate@gmail.com>
2023-12-04 17:48:25 +0800

    updated api link

--- megamenu-v2/src/main.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-
-export default (initialState: any, el: HTMLElement) => {
-  ReactDOM.hydrate(<App data={initialState.data} />, el);
-};
