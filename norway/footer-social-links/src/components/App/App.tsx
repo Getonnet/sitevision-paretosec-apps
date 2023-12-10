a4ee494f7e25aca8d1308f4b4fd14a709249af21
walidmahade <walidmahade@gmail.com>
2023-12-10 21:28:44 +0600

    fix footer focial links

--- norway/footer-social-links/src/components/App/App.tsx
+++ norway/footer-social-links/src/components/App/App.tsx
@@ -29,32 +29,62 @@ const App: React.FunctionComponent<AppProperties> = ({
   return (
     <div className={styles.container}>
       {FacebookIconLink ? (
-        <a href={FacebookIconLink} target="_blank" className={styles.icon}>
+        <a
+          href={FacebookIconLink}
+          target="_blank"
+          className={styles.icon}
+          aria-label={"FacebookIcon"}
+        >
           <FacebookIcon />
         </a>
       ) : null}
       {TwitterIconLink ? (
-        <a href={TwitterIconLink} target="_blank" className={styles.icon}>
+        <a
+          href={TwitterIconLink}
+          target="_blank"
+          className={styles.icon}
+          aria-label={"TwitterIcon"}
+        >
           <TwitterIcon />
         </a>
       ) : null}
       {LinkedinIconLink ? (
-        <a href={LinkedinIconLink} target="_blank" className={styles.icon}>
+        <a
+          href={LinkedinIconLink}
+          target="_blank"
+          className={styles.icon}
+          aria-label={"LinkedinIcon"}
+        >
           <LinkedinIcon />
         </a>
       ) : null}
       {ThreadsIconLink ? (
-        <a href={ThreadsIconLink} target="_blank" className={styles.icon}>
+        <a
+          href={ThreadsIconLink}
+          target="_blank"
+          className={styles.icon}
+          aria-label={"ThreadsIcon"}
+        >
           <ThreadsIcon />
         </a>
       ) : null}
       {InstagramIconLink ? (
-        <a href={InstagramIconLink} target="_blank" className={styles.icon}>
+        <a
+          href={InstagramIconLink}
+          target="_blank"
+          className={styles.icon}
+          aria-label={"InstagramIcon"}
+        >
           <InstagramIcon />
         </a>
       ) : null}
       {YoutubeIconLink ? (
-        <a href={YoutubeIconLink} target="_blank" className={styles.icon}>
+        <a
+          href={YoutubeIconLink}
+          target="_blank"
+          className={styles.icon}
+          aria-label={"YoutubeIcon"}
+        >
           <YoutubeIcon />
         </a>
       ) : null}
