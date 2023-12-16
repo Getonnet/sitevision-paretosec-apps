eaa4d0d9578b5bcde278454c707e4ee4f8123a00
walidmahade <walidmahade@gmail.com>
2023-12-16 10:31:44 +0600

    update slug for tickers

--- norway/megamenu-v2/src/actions/getTickers.tsx
+++ norway/megamenu-v2/src/actions/getTickers.tsx
@@ -20,8 +20,3 @@ export const getTickers = async () => {
 
   return result;
 };
-
-// const REST_API = "/rest-api/1/0";
-// const PAGE_REPOSITORY = "/19.1dbe752318bf2416daa26d2";
-// const API_URL_SUFFIX = "/nodes?format=json&json=%7B%22properties%22%3A%5B%22*%22%5D%7D";
-// const PATH_TICKERS = "/Tickers";
--- sweden/megamenu-v2/src/actions/getTickers.tsx
+++ sweden/megamenu-v2/src/actions/getTickers.tsx
@@ -1,27 +1,20 @@
 import requester from "@sitevision/api/client/requester";
 
 export const getTickers = async () => {
-  const REST_API = "/rest-api/1/0";
-  const PAGE_REPOSITORY = "/19.1dbe752318bf2416daa26d2";
-  const API_URL_SUFFIX =
-    "/nodes?format=json&json=%7B%22properties%22%3A%5B%22*%22%5D%7D";
-  const PATH_TICKERS = "/Tickers";
+  const TICKER_ENDPOINT = "/rest-api/1/0/19.17e5c29718bfae2e60117c93/nodes";
+  const PAYLOAD = { properties: ["*"] };
 
-  let result: any = []
+  let result: any = [];
 
   await requester
     .doGet({
-      url:
-        REST_API +
-        PAGE_REPOSITORY +
-        PATH_TICKERS +
-        API_URL_SUFFIX,
+      url: TICKER_ENDPOINT,
     })
     .then((response) => {
       result = response;
     })
     .catch((error) => {
-      console.log(error)
+      console.log(error);
     });
 
   return result;
