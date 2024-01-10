862a4a036335f57f97eae306a7e133e4351fa04c
walidmahade <walidmahade@gmail.com>
2024-01-10 20:16:01 +0600

    add signed version

--- norway/redirects/src/config/index.js
+++ norway/redirects/src/config/index.js
@@ -12,43 +12,41 @@
   // const mainPageNode = resourceLocatorUtil.getSitePage();
   const db = storage.getKeyValueDataStore("paretosec-dev");
 
+  router.get("/", (req, res) => {
+    res.render({});
+  });
+
   const getFormattedValues = (string) => {
     const mergedObject = {};
     const lines = string.split("\n");
     lines.map((line) => {
       const lineArr = line.split(",").map((item) => item.trim());
+      // removing domain name, so redirects will work even if domain changes
+      let nodeUrl = lineArr[1].replace("https://www.paretosec.no/", "/");
+      nodeUrl = nodeUrl.replace("https://paretosec.no/", "/");
+
       if (mergedObject.hasOwnProperty(lineArr[1])) {
-        mergedObject[lineArr[1]] = [...mergedObject[lineArr[1]], lineArr[0]];
+        mergedObject[nodeUrl] = [...mergedObject[lineArr[1]], lineArr[0]];
       } else {
-        mergedObject[lineArr[1]] = [lineArr[0]];
+        mergedObject[nodeUrl] = [lineArr[0]];
       }
     });
     return mergedObject;
   };
 
-  router.get("/", (req, res) => {
-    res.render({});
-  });
-
-  /**
-   * TODO: Merge array of string into, {key$: [string]}
-   * Current implementation will send too many request to same node
-   * Redirect util lets us add multiple path to one node,
-   * Lets add in bulks
-   */
   router.post("/add-redirect-uris", (req, res) => {
     const urlsObj = getFormattedValues(req.params.value);
     const newNodes = [];
 
     async function addRedirectsWithDelay() {
       for (const key in urlsObj) {
-        // console.log(`${key}: ${urlsObj[key]}`);
-        const url = urlsObj[key];
-        const targetNode = resourceLocatorUtil.getNodeByUrl(url[1]);
+        const urlsArr = urlsObj[key];
+        const targetNode = resourceLocatorUtil.getNodeByUrl(key);
+
         if (targetNode) {
-          newNodes.indexOf(url[1]) === -1 && newNodes.push(url[1]);
-          redirectUtil.addRedirectURI(targetNode, url[0]);
-          await delay(1000); // Adjust the delay time in milliseconds (e.g., 1000ms = 1s)
+          newNodes.indexOf(key) === -1 && newNodes.push(key);
+          redirectUtil.addRedirectURIs(targetNode, urlsArr);
+          await delay(500);
         }
       }
     }
@@ -59,32 +57,35 @@
 
     // Call the function with your urlArr
     try {
-      addRedirectsWithDelay().then(() => {});
-      // get old nodes
-      const existingNodes = db.get("redirectNodes");
+      addRedirectsWithDelay().then(() => {
+        // get old nodes
+        const existingNodes = db.get("redirectNodes");
 
-      // merge old nodes with new nodes
-      let mergedNodes = {};
-      if (existingNodes) {
-        mergedNodes = {
-          nodes: [...new Set([...newNodes, ...existingNodes.nodes])],
-        };
-      } else {
-        mergedNodes = {
-          nodes: newNodes,
-        };
-      }
+        // merge old nodes with new nodes
+        let mergedNodes = {};
+        if (existingNodes) {
+          mergedNodes = {
+            nodes: [...new Set([...newNodes, ...existingNodes.nodes])],
+          };
+        } else {
+          mergedNodes = {
+            nodes: newNodes,
+          };
+        }
 
-      // save to db
-      db.put("redirectNodes", mergedNodes);
-      // send back response
-      res.send("Success");
+        // save to db
+        db.put("redirectNodes", mergedNodes);
+
+        // send back response
+        res.send("Success");
+      });
     } catch (e) {
       res.send(JSON.stringify(e));
     }
   });
 
   router.get("/all-redirects", () => {
+    const existingNodes = db.get("redirectNodes");
     // redirectUtil.getRedirectURIs();
   });
 
