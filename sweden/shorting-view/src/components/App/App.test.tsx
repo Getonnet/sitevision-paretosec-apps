e0cb2f8306b849e6cb287aecb29271e7ac5ab68f
walidmahade <walidmahade@gmail.com>
2024-01-11 10:35:35 +0600

    add signing

--- sweden/shorting-view/src/components/App/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { screen } from "@testing-library/react";
-
-test("renders correct message and name", () => {
-  // render(<App message="Hello," name="World" />);
-  const element = screen.getByText(/hello, world/i);
-  expect(element).toBeInTheDocument();
-});
