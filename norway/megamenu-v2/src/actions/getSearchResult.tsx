589221915d543275338e8ae3b101107a17fd45cc
Eric <ericjohnariate@gmail.com>
2024-01-12 17:34:49 +0800

    updated search request url

--- norway/megamenu-v2/src/actions/getSearchResult.tsx
+++ norway/megamenu-v2/src/actions/getSearchResult.tsx
@@ -1,20 +1,20 @@
 import requester from "@sitevision/api/client/requester";
 
 export const getSearchResult = async (term: string) => {
-    const SEARCH_URL = `/2.54e1ff71188bd8464773cc67/12.4ca3d05b18bd10771975ec.json?state=autoComplete&term=${term}`;
+  const SEARCH_URL = `/4.1ec403e918a983e77b927259/12.1ec403e918a983e77b927739.json?state=autoComplete&term${term}`;
 
-    let result: any = []
+  let result: any = [];
 
-    await requester
-        .doGet({
-            url:  SEARCH_URL,
-        })
-        .then(response => {
-            result = response;
-        })
-        .catch(error => {
-            result = ['Ingen resultater.'];
-        });
+  await requester
+    .doGet({
+      url: SEARCH_URL,
+    })
+    .then((response) => {
+      result = response;
+    })
+    .catch((error) => {
+      result = ["Ingen resultater."];
+    });
 
-    return result
-} 
+  return result;
+};
