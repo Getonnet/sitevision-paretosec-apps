5ea7527eb1e483021708524c77376391065cfd81
Eric <ericjohnariate@gmail.com>
2024-01-12 17:42:58 +0800

    Updated search request url

--- norway/megamenu-v2/src/actions/getSearchResult.tsx
+++ norway/megamenu-v2/src/actions/getSearchResult.tsx
@@ -1,7 +1,7 @@
 import requester from "@sitevision/api/client/requester";
 
 export const getSearchResult = async (term: string) => {
-  const SEARCH_URL = `/4.1ec403e918a983e77b927259/12.1ec403e918a983e77b927739.json?state=autoComplete&term${term}`;
+  const SEARCH_URL = `/4.1ec403e918a983e77b927259/12.1ec403e918a983e77b927739.json?state=autoComplete&term=${term}`;
 
   let result: any = [];
 
